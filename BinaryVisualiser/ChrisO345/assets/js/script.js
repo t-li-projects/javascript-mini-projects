c24f0ec51bfdecc2788b32dce240e664cd9baf56
t-li-projects <130779164+t-li-projects@users.noreply.github.com>
2023-08-08 21:49:39 +0200

    Improved looks and JS overhaul
    Improved UI, complete rework of JS, clearer CSS, the layout and functionality remain the same, but with minor improvements. Added comments for clarification.
    
    HTML & CSS: complete overhaul the id & class system, resulting in a more concise document. Additional colour styling and added numbering system labels. The tool is now centred.
    
    JS: completely new document, previous approach was very over-engineered  and complex to understand. All levers execute the exact same function, which relies on the HTML 'value' attribute to tell levers apart. after each lever click, the outputs are updated using the `updateOutput()` function.

--- BinaryVisualiser/ChrisO345/assets/js/script.js
+++ BinaryVisualiser/ChrisO345/assets/js/script.js
@@ -1,105 +1,46 @@
-var nought1 = 0;
-var nought2 = 0;
-var nought3 = 0;
-var nought4 = 0;
-var nought5 = 0;
-var nought6 = 0;
-var nought7 = 0;
-var nought8 = 0;
+// lever imgs
+const levers = document.querySelectorAll('.lever')
 
-function changeLeverOne() {
-  if (nought1 == 0) {
-    document.getElementById('firstLever').src = 'assets/img/lever_down.png';
-    nought1 = 1;
-  }
-  else {
-    document.getElementById("firstLever").src = 'assets/img/lever_up.png';
-    nought1 = 0;
-  }
-  workOut()
-}
-function changeLeverTwo() {
-  if (nought2 == 0) {
-    document.getElementById('secondLever').src = 'assets/img/lever_down.png';
-    nought2 = 1;
-  }
-  else {
-    document.getElementById("secondLever").src = 'assets/img/lever_up.png';
-    nought2 = 0;
-  }
-  workOut()
-}
-function changeLeverThree() {
-  if (nought3 == 0) {
-    document.getElementById('thirdLever').src = 'assets/img/lever_down.png';
-    nought3 = 1;
-  }
-  else {
-    document.getElementById("thirdLever").src = 'assets/img/lever_up.png';
-    nought3 = 0;
-  }
-  workOut()
-}
-function changeLeverFour() {
-  if (nought4 == 0) {
-    document.getElementById('fourthLever').src = 'assets/img/lever_down.png';
-    nought4 = 1;
-  }
-  else {
-    document.getElementById("fourthLever").src = 'assets/img/lever_up.png';
-    nought4 = 0;
-  }
-  workOut()
-}
-function changeLeverFive() {
-  if (nought5 == 0) {
-    document.getElementById('fifthLever').src = 'assets/img/lever_down.png';
-    nought5 = 1;
-  }
-  else {
-    document.getElementById("fifthLever").src = 'assets/img/lever_up.png';
-    nought5 = 0;
-  }
-  workOut()
-}
-function changeLeverSix() {
-  if (nought6 == 0) {
-    document.getElementById('sixthLever').src = 'assets/img/lever_down.png';
-    nought6 = 1;
-  }
-  else {
-    document.getElementById("sixthLever").src = 'assets/img/lever_up.png';
-    nought6 = 0;
-  }
-  workOut()
-}
-function changeLeverSeven() {
-  if (nought7 == 0) {
-    document.getElementById('seventhLever').src = 'assets/img/lever_down.png';
-    nought7 = 1;
-  }
-  else {
-    document.getElementById("seventhLever").src = 'assets/img/lever_up.png';
-    nought7 = 0;
-  }
-  workOut()
-}
-function changeLeverEight() {
-  if (nought8 == 0) {
-    document.getElementById('eighthLever').src = 'assets/img/lever_down.png';
-    nought8 = 1;
-  }
-  else {
-    document.getElementById("eighthLever").src = 'assets/img/lever_up.png';
-    nought8 = 0;
-  }
-  workOut()
-}
-function workOut() {
-  document.getElementById('binary').innerHTML = nought1.toString()+nought2.toString()+nought3.toString()+nought4.toString();
-  document.getElementById('binary2').innerHTML = nought5.toString()+nought6.toString()+nought7.toString()+nought8.toString();
-  document.getElementById('hexidecimal').innerHTML = ((nought1*8+nought2*4+nought3*2+nought4*1).toString(16)).toUpperCase();
-  document.getElementById('hexidecimal2').innerHTML = ((nought5*8+nought6*4+nought7*2+nought8*1).toString(16)).toUpperCase();
-  document.getElementById('decimal').innerHTML = (nought1*128+nought2*64+nought3*32+nought4*16+nought5*8+nought6*4+nought7*2+nought8*1).toString()
-  // Add octal at one point???
+// output cells
+const binaryOut = document.querySelector('#binary')
+const decimalOut = document.querySelector('#decimal')
+const hexadecimalOut = document.querySelector('#hexadecimal')
+
+// array of the values of currently active levers
+const activeLevers = []
+
+
+// listen for 'click' on all levers
+levers.forEach(lever => {
+  lever.addEventListener('click', (e) => {
+
+    // declare lever and its value
+    const lever = e.target
+    const leverValue = lever.getAttribute('value')
+
+    //  if value of current lever is in `activeLevers`, remove it from the array
+    if (activeLevers.includes(leverValue)) {
+      lever.src = 'assets/img/lever_up.png' // turn lever up
+      activeLevers.splice(activeLevers.indexOf(leverValue), 1)
+    } else {
+      // if value of current lever is not in `activeLevers`, add it to the array
+      lever.src = 'assets/img/lever_down.png' // turn lever down
+      activeLevers.push(leverValue)
+    }
+
+    // update output cells
+    updateOutput()
+  })
+})
+
+
+const updateOutput = () => {
+
+  // get decimal value by summing up elements from `activeLevers` using reduce
+  const valueDecimal = activeLevers.reduce((sum, cur) => sum + parseInt(cur), 0)
+
+  // change the text content of output cells accordingly, `.toString(n)` is used to convert to different systems
+  binaryOut.textContent = valueDecimal.toString('2')
+  decimalOut.textContent = valueDecimal
+  hexadecimalOut.textContent = valueDecimal.toString('16').toUpperCase()
 }
